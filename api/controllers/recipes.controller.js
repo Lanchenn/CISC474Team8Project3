6d94dc0a2d82acd01708965c0023b64332eafe89
Admin <Admin@Estellas-MacBook-Pro.local>
2017-11-29 15:46:31 -0500

    pushing recipe api changes/new components

--- api/controllers/recipes.controller.js
+++ api/controllers/recipes.controller.js
@@ -1,6 +1,7 @@
 const Recipe=require('../models/recipe');
 
 exports.getRecipes=function(req,res,next){
+    console.log('in general get recipes function');
     Recipe.find({},function(err,recipes){
         res.status(201).json(recipes)
     })
@@ -14,12 +15,7 @@ exports.createRecipe=function(req,res,next){
     // res.status(200).json({
     //     test: 'Good to go2'
     // });
-    // console.log('req.body=');
-    // console.log(req.body);
-    // console.log('req body title=');
-    // console.log(req.body.title);
-    // console.log('u = ');
-    // console.log(u);
+    
     var u=new Recipe({title: req.body.title, owner:req.body.owner, 
         description:req.body.description, ingredients:req.body.ingredients, steps:req.body.steps,
         mealType:req.body.mealType, worldCuisine:req.body.worldCuisine}); //don't include rating on create
@@ -30,7 +26,23 @@ exports.createRecipe=function(req,res,next){
 
 
 }
-exports.getRecipe=function(req,res,next){
-    res.status(200).json({result: 'not implemented'})
+//implement this Monday
+exports.getRecipe=function(id){
+    console.log('in find recipe by id function');
+    //return 'here';
+    console.log(recipe.find(item => item.id == id));
+
+    return recipe.find(item => item.id == id);
+
+    // if(!recipe[id]){
+    //     return callback(new Error(
+    //         'No recipe with id ' + id
+    //     )
+    // );
+    // return callback(null, recipe[id]);
+    // }
+
+    //    return Recipe.find(x => x.id == req.body)
+    //res.status(200).json({result: 'not implemented'})
 
 }
