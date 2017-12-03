b445508b6612841fba18990caa42cfd9ba03a2d6
Admin <Admin@Estellas-MacBook-Pro.local>
2017-12-02 21:21:06 -0500

    Progress on ID retrieval. Setup for funcs that will be needed later on.

--- api/controllers/recipes.controller.js
+++ api/controllers/recipes.controller.js
@@ -1,16 +1,59 @@
 const Recipe=require('../models/recipe');
 
+
+
+//Current focus is this function:
+//Request correctly routes here now, but passess null...
+//Need to fix.
+exports.getRecipeByID=function(req, res, next){
+    console.log('in find recipe by id function');
+
+    //console.log('req= '+ req + ' res= '+res+' next= '+next);
+
+   // console.log('id = '+req.body);
+    //console.log(Recipe.find(item => item.id == id));
+
+    return Recipe.find(item => item.id == id);
+
+    // if(!recipe[id]){
+    //     return callback(new Error(
+    //         'No recipe with id ' + id
+    //     )
+    // );
+    // return callback(null, recipe[id]);
+    // }
+
+    //    return Recipe.find(x => x.id == req.body)
+    //res.status(200).json({result: 'not implemented'})
+}
+
+//Some definitions. When get by ID func complete, implement these in similar fashion
+//double-check schema var names 1st if bugs occur
+exports.getRecipeByMealType=function(mealType){
+    console.log('in find recipe by meal type function');
+    return Recipe.find(item => item.mealType == mealType);
+}
+
+exports.getRecipeByAuth=function(author){
+    console.log('in find recipe by author function');
+    return Recipe.find(item => item.author == author);
+}
+exports.getRecipeByWorld=function(wC){
+    console.log('in find recipe by author function');
+    return Recipe.find(item => item.worldCuisine == wC);
+}
+
+
+//-------------------------------------------------------------
+//Works
 exports.getRecipes=function(req,res,next){
     console.log('in general get recipes function');
     Recipe.find({},function(err,recipes){
         res.status(201).json(recipes)
     })
 }
-exports.test=function(req,res,next){
-    res.status(200).json({
-        test: 'Good to go'
-    });
-}
+
+//Works
 exports.createRecipe=function(req,res,next){
     // res.status(200).json({
     //     test: 'Good to go2'
@@ -26,23 +69,9 @@ exports.createRecipe=function(req,res,next){
 
 
 }
-//implement this Monday
-exports.getRecipe=function(id){
-    console.log('in find recipe by id function');
-    //return 'here';
-    console.log(recipe.find(item => item.id == id));
-
-    return recipe.find(item => item.id == id);
-
-    // if(!recipe[id]){
-    //     return callback(new Error(
-    //         'No recipe with id ' + id
-    //     )
-    // );
-    // return callback(null, recipe[id]);
-    // }
-
-    //    return Recipe.find(x => x.id == req.body)
-    //res.status(200).json({result: 'not implemented'})
-
+//Works
+exports.test=function(req,res,next){
+    res.status(200).json({
+        test: 'Good to go'
+    });
 }
--- api/router.js
+++ api/router.js
@@ -19,7 +19,11 @@ module.exports = function(app) {
 
     //use these to test new stuff (don't forget to add proper prefix - see below)
     recipesRoutes.get('/', recipeController.getRecipes);
-    recipesRoutes.get('/:id', recipeController.getRecipe);
+    recipesRoutes.get('/:id', recipeController.getRecipeByID); //get recipe with id=id
+    recipesRoutes.get('/author/:authID', recipeController.getRecipeByAuth); //get all recipes written by user with id=authID
+    recipesRoutes.get('/type/:myType', recipeController.getRecipeByMealType); //get recipes with mealtype = myType
+    recipesRoutes.get('/world/:worldType', recipeController.getRecipeByWorld=); //get recipes with worldCuisine = worldType
+
     recipesRoutes.post('/', recipeController.createRecipe);
 
     authenticationRoutes.post('/register', authenticationController.register);
