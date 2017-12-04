be14779118f7aec8e6d9d59c3e9b053893f7a4c1
Admin <Admin@Estellas-MacBook-Pro.local>
2017-12-03 21:14:14 -0500

    implementation of get-by functions for recipes. Some things to debug, but usable for the moment

--- api/controllers/recipes.controller.js
+++ api/controllers/recipes.controller.js
@@ -1,46 +1,69 @@
 const Recipe=require('../models/recipe');
 
 
-
 //Current focus is this function:
-//Request correctly routes here now, but passess null...
-//Need to fix.
-exports.getRecipeByID=function(req, res, next){
+//Debug: returns nothing - err 450 obj doesn't exist, even w/ verified data. Problem w/ DB?
+exports.getRecipeByID = function (req, res, next) {
     console.log('in find recipe by id function');
-
-    //console.log('req= '+ req + ' res= '+res+' next= '+next);
-
-   // console.log('id = '+req.body);
-    //console.log(Recipe.find(item => item.id == id));
-
-    return Recipe.find(item => item.id == id);
-
-    // if(!recipe[id]){
-    //     return callback(new Error(
-    //         'No recipe with id ' + id
-    //     )
-    // );
-    // return callback(null, recipe[id]);
-    // }
-
-    //    return Recipe.find(x => x.id == req.body)
-    //res.status(200).json({result: 'not implemented'})
+    const id = req.params['id'];
+    return Recipe.findOne({ id: id }, function (err, obj) {
+        if (err) {
+            res.status(400).send(err);
+        }
+        else if (!obj){
+            res.status(450).json({});
+        }
+        else {
+            res.status(200).json(obj);
+        }
+    });
 }
 
-//Some definitions. When get by ID func complete, implement these in similar fashion
-//double-check schema var names 1st if bugs occur
-exports.getRecipeByMealType=function(mealType){
+exports.getRecipeByMealType= function (req, res, next) {
     console.log('in find recipe by meal type function');
-    return Recipe.find(item => item.mealType == mealType);
+    const mealType = req.params['mealType'];
+    return Recipe.findOne({ mealType: mealType }, function (err, obj) {
+        if (err) {
+            res.status(400).send(err);
+        }
+        else if (!obj){
+            res.status(450).json({});
+        }
+        else {
+            res.status(200).json(obj);
+        }
+    });
 }
 
-exports.getRecipeByAuth=function(author){
+exports.getRecipeByAuth= function (req, res, next) {
     console.log('in find recipe by author function');
-    return Recipe.find(item => item.author == author);
+    const author = req.params['author'];
+    return Recipe.findOne({ author: author }, function (err, obj) {
+        if (err) {
+            res.status(400).send(err);
+        }
+        else if (!obj){
+            res.status(450).json({});
+        }
+        else {
+            res.status(200).json(obj);
+        }
+    });
 }
-exports.getRecipeByWorld=function(wC){
+exports.getRecipeByWorld= function (req, res, next) {
     console.log('in find recipe by author function');
-    return Recipe.find(item => item.worldCuisine == wC);
+    const worldCuisine = req.params['worldCuisine'];
+    return Recipe.findOne({ worldCuisine: worldCuisine }, function (err, obj) {
+        if (err) {
+            res.status(400).send(err);
+        }
+        else if (!obj){
+            res.status(450).json({});
+        }
+        else {
+            res.status(200).json(obj);
+        }
+    });
 }
 
 
