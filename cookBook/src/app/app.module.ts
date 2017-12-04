d1d220286805cd2fff7d67a599c2259fe3277ed6
lanchenn <allielc@udel.edu>
2017-12-04 13:08:03 -0500

    home page html

--- cookBook/src/app/app.module.ts
+++ cookBook/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewsComponent } from './views/views.component';
 import { ModulesComponent } from './modules/modules.component';
+import { RecipeComponent } from './views/recipe/recipe.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ViewsComponent,
-    ModulesComponent
+    ModulesComponent,
+    RecipeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
