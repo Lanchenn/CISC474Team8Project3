9366ea4f221f04bca2b60d5eb6fe6e417e2f9dfd
lanchenn <allielc@udel.edu>
2017-12-08 00:16:21 -0500

    home page done/nev bar done

--- cookBook/src/app/app-routing.module.ts
+++ cookBook/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {ViewsComponent} from './views/views.component';
+import {HomeComponent} from './views/home/home.component';
+import {RecipeComponent} from './views/recipe/recipe.component';
+import {SigninComponent} from './views/signin/signin.component';
+import {SignupComponent} from './views/signup/signup.component';
+
+
+const routes: Routes = [
+//  {path:'',redirectTo:'app',pathMatch:'full'},
+  {path:'',redirectTo:'views',pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'recipe',component:RecipeComponent},
+  {path:'signin',component:SigninComponent},
+  {path:'signup',component:SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cookBook/src/app/app.module.ts
+++ cookBook/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//import {BootstrapModalModule} from 'ng2-bootstrap-modal';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewsComponent } from './views/views.component';
 import { ModulesComponent } from './modules/modules.component';
 import { RecipeComponent } from './views/recipe/recipe.component';
 import { HomeComponent } from './views/home/home.component';
+import { SigninComponent } from './views/signin/signin.component';
+import { SignupComponent } from './views/signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +16,14 @@ import { HomeComponent } from './views/home/home.component';
     ViewsComponent,
     ModulesComponent,
     RecipeComponent,
-    HomeComponent
+    HomeComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    //BootstrapModalModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
